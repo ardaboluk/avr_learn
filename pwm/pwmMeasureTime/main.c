b0976c9925ec304acbe4265ede13f7ff3de04947
ardaboluk <ardaboluk2@gmail.com>
2018-11-02 09:25:48 +0300

    Fixed timer initialization function to set the clock prescaler to /256. Added comments that explain the causes of issues.

--- pwm/pwmMeasureTime/main.c
+++ pwm/pwmMeasureTime/main.c
@@ -15,11 +15,20 @@
 #define LED2 PB2
 #define LED3 PB3
 
-// if we give 20 for LED_DELAY_us and 255 for BRIGHTNESS_UPPER_LIMIT, timer shows 493 ms have been passed between dimmest and brightest
-// if we give 5 for LED_DELAY_us and 510 for BRIGHTNESS_UPPER_LIMIT, timer shows 1083 ms have been passed between dimmest and brightest
-// Firstly, these times are inconsistent with the values that we calculate (20*255*255 us = 1300 ms and 5*510*510 us = 1300 ms).
-// Also, the second setting takes approximately twice as long as the first setting.
-// Should solve these mysteries :o
+/*  if we give 20 for LED_DELAY_us and 255 for BRIGHTNESS_UPPER_LIMIT, timer shows 1975 ms have been passed between dimmest and brightest
+*   if we give 5 for LED_DELAY_us and 510 for BRIGHTNESS_UPPER_LIMIT, timer shows 4332 ms have been passed between dimmest and brightest
+*   Firstly, these times are inconsistent with the values that we calculate (20*255*255 us = 1300 ms and 5*510*510 us = 1300 ms).
+*   Also, the second setting takes approximately twice as long as the first setting.
+*
+*   It's more appropriate to resolve the second issue first. It's true that in both of the settings delay_us makes the system wait equal amounts of time.
+*   However, in the second setting the if condition in pwmAllPins is checked 510 * 510 times in total. Also, the expression in the if block is called 510 times in total.
+*   In the first setting the if condition is checked 255 * 255 times. The expression in the if block is called 255 times. So if we calculate these roughly m+n calls for
+*   the first setting, it would be 4m+2n calls for the second setting. This gives a direction as to why the second setting takes slightly more than twice time as the first setting.
+*
+*   The first issue is caused also by this problem of overlooking the time if block takes. It should take 1300 ms. But the if block (together with the expression inside) takes some time.
+*   In the second setting, it takes further time because of the larger number of calls.
+*/
+
 #define LED_DELAY_us  5
 #define BRIGHTNESS_UPPER_LIMIT 510
 
--- pwm/pwmMeasureTime/my_timer.c
+++ pwm/pwmMeasureTime/my_timer.c
@@ -6,5 +6,5 @@ void initTimer1(void){
     // set the prescaler to /256
     // CPU speed is defined as 1MHz in the makefile, thus the speed of the clock
     // is (1/256)MHz. Each tick is 256 microseconds long.
-    TCCR1B = (1 << CS12) | (1 << CS10);
+    TCCR1B = (1 << CS12);
 }
